d33ad3731cb934a9d2f82fadc8c1a45c398f99c0
Mayank-MP05 <mayank5pande@gmail.com>
2020-09-25 18:50:45 +0530

    Navbar Styling and Linking

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import hulkLogo from "./../assets/hulk-icon.png";
 
 function Navbar() {
-  return <div>This is sample navbar</div>;
+  return (
+    <nav className='navbar navbar-light bg-light'>
+      <Link className='navbar-brand d-flex flex-row' to='/home'>
+        <img
+          src={hulkLogo}
+          className='d-inline-block align-top hulk-logo'
+          alt='Main App Hulk Logo'
+        />
+        <ul className='navbar-nav'>
+          <li class='nav-item active mx-3'>
+            <Link class='nav-link' to='/home'>
+              Hulk-Huffman Compressor
+            </Link>
+          </li>
+        </ul>
+      </Link>
+      <ul class='navbar-nav ml-auto'>
+        <li class='nav-item active mx-3'>
+          <Link class='nav-link' to='/about'>
+            About <span class='sr-only'>(current)</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
