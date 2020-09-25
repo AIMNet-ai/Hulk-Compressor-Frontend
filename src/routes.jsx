a2d74a56dc6b516716224dba1e808fb32c3af600
Mayank-MP05 <mayank5pande@gmail.com>
2020-09-25 19:29:24 +0530

    New route for Tab testing

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/about";
+import Tabbed from "./components/tabbed";
 
 function Routes() {
   return (
     <Switch>
       <Route exact path='/home' component={Home} />
+      <Route exact path='/tabbed' component={Tabbed} />
       <Route exact path='/about' component={About} />
       <Redirect from='/' to='/home' />
     </Switch>
