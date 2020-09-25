2965f1974c57c5271273c2a5cfaa9ba006f0fdb8
Mayank-MP05 <mayank5pande@gmail.com>
2020-09-25 18:40:56 +0530

    Main Root of the react home

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import logo from "./assets/hulk.png";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Routes from "./routes";
 
 function App() {
   const [count, setcount] = useState(0);
   return (
+    <Router>
+      <Navbar />
+      <Routes />
+    </Router>
+  );
+}
+
+export default App;
+/*
+
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
@@ -16,7 +29,5 @@ function App() {
         </button>
       </header>
     </div>
-  );
-}
 
-export default App;
+*/
