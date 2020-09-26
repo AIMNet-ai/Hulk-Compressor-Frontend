ff151795244626ef88597889bf0969837ce9a67b
Mayank-MP05 <mayank5pande@gmail.com>
2020-09-26 08:05:58 +0530

    Showing Tree and Modal over request

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -17,6 +17,9 @@ function Home(props) {
   const [encoderOrDecoder, setencoderOrDecoder] = useState(1);
   const [DEC, setDEC] = useState("asasssddssdsdaa");
   const [ENC, setENC] = useState("10011011110001111010");
+  const [tree, settree] = useState({});
+  const [showtree, setshowtree] = useState(false);
+  const [result, setresult] = useState({});
 
   const checkValidBinary = (str) => {
     const arr = str.split("");
@@ -37,6 +40,7 @@ function Home(props) {
           .then((res) => {
             console.log(res.data);
             setENC(res.data.output);
+            setresult(res.data);
           });
       }
     } else {
@@ -49,9 +53,18 @@ function Home(props) {
           .then((res) => {
             console.log(res.data);
             setDEC(res.data.output);
+            setresult(res.data);
           });
       }
     }
+  };
+
+  const seeHuffmanTree = () => {
+    axios.get("http://localhost:5000/api/get-tree").then((res) => {
+      settree(res.data.tree);
+      setshowtree(true);
+      window.scrollTo(0, document.body.scrollHeight);
+    });
   };
 
   const onChangeText = (e) => {
@@ -75,131 +88,153 @@ function Home(props) {
   };
 
   return (
-    <div className='row p-3'>
-      <div className='col-md-5'>
-        <div className='card border-round'>
-          <div className='d-flex flex-row align-items-center'>
-            <div className='d-flex flex-column p-2'>
-              <h5 className='text-bold'>Input Text</h5>
-              <small>
-                {encoderOrDecoder
-                  ? "(Simple Plain English Text)"
-                  : "(Huffman Endoded Binary Text)"}
-              </small>
-            </div>
-
-            <img
-              src={encoderOrDecoder ? textFileIcon : binaryFileIcon}
-              alt=''
-              className='right-circled'
-            />
-          </div>
-          <hr className='w-75 m-auto' />
-          {textOrFile ? (
-            <textarea
-              cols='30'
-              rows='15'
-              name='DEC'
-              onChange={onChangeText}
-              value={encoderOrDecoder ? DEC : ENC}></textarea>
-          ) : (
-            <form className='w-100 d-flex flex-column align-items-center justify-content-center'>
-              <img src={textFileIcon} className='m-auto p-2 w-75' alt='' />
-
-              <div className='form-group'>
-                <input
-                  type='file'
-                  className='form-control-file'
-                  id='exampleFormControlFile1'
-                />
-                <small id='emailHelp' className='form-text text-muted'>
-                  Only text file allowed to upload.
+    <>
+      <div className='row p-3'>
+        <div className='col-md-5'>
+          <div className='card border-round'>
+            <div className='d-flex flex-row align-items-center'>
+              <div className='d-flex flex-column p-2'>
+                <h5 className='text-bold'>Input Text</h5>
+                <small>
+                  {encoderOrDecoder
+                    ? "(Simple Plain English Text)"
+                    : "(Huffman Endoded Binary Text)"}
                 </small>
               </div>
-              <div className='p-1'>
-                <button type='submit' className='btn btn-primary m-auto'>
-                  Upload File
-                </button>
-              </div>
-            </form>
-          )}
+
+              <img
+                src={encoderOrDecoder ? textFileIcon : binaryFileIcon}
+                alt=''
+                className='right-circled'
+              />
+            </div>
+            <hr className='w-75 m-auto' />
+            {textOrFile ? (
+              <textarea
+                cols='30'
+                rows='15'
+                name='DEC'
+                onChange={onChangeText}
+                value={encoderOrDecoder ? DEC : ENC}></textarea>
+            ) : (
+              <form className='w-100 d-flex flex-column align-items-center justify-content-center'>
+                <img src={textFileIcon} className='m-auto p-2 w-75' alt='' />
+
+                <div className='form-group'>
+                  <input
+                    type='file'
+                    className='form-control-file'
+                    id='exampleFormControlFile1'
+                  />
+                  <small id='emailHelp' className='form-text text-muted'>
+                    Only text file allowed to upload.
+                  </small>
+                </div>
+                <div className='p-1'>
+                  <button type='submit' className='btn btn-primary m-auto'>
+                    Upload File
+                  </button>
+                </div>
+              </form>
+            )}
+          </div>
         </div>
-      </div>
-      <div className='col-md-2 d-flex flex-column align-items-center justify-content-center'>
-        {textOrFile ? (
+        <div className='col-md-2 d-flex flex-column align-items-center justify-content-center'>
+          {textOrFile ? (
+            <button
+              className='btn btn-outline-info m-2 middle-btn'
+              onClick={() => {
+                let next = encoderOrDecoder === 1 ? 0 : 1;
+                setencoderOrDecoder(next);
+              }}>
+              Revert Selection
+            </button>
+          ) : (
+            ""
+          )}
           <button
             className='btn btn-outline-info m-2 middle-btn'
-            onClick={() => {
-              let next = encoderOrDecoder === 1 ? 0 : 1;
-              setencoderOrDecoder(next);
-            }}>
-            Revert Selection
+            onClick={calculateOperation}>
+            {encoderOrDecoder ? "Encode" : "Decode"}
           </button>
-        ) : (
-          ""
-        )}
-        <button
-          className='btn btn-outline-info m-2 middle-btn'
-          onClick={calculateOperation}>
-          {encoderOrDecoder ? "Encode" : "Decode"}
-        </button>
-        {textOrFile ? (
-          <button className='btn btn-outline-info m-2 middle-btn'>
-            Draw Huffman Tree
-          </button>
-        ) : (
-          ""
-        )}
-        {textOrFile ? (
-          <button
-            className='btn btn-outline-danger m-2 middle-btn'
-            onClick={clearBoth}>
-            Clear both
-          </button>
-        ) : (
-          ""
-        )}
-      </div>
-      <div className='col-md-5'>
-        <div className='card border-round'>
-          <div className='d-flex flex-row align-items-center'>
-            <div className='d-flex flex-column p-2'>
-              <h5 className='text-bold'>Output Text</h5>
-              <small>
-                {!encoderOrDecoder
-                  ? "(Simple Plain English Text)"
-                  : "(Huffman Endoded Binary Text)"}
-              </small>
-            </div>
-            <img
-              src={encoderOrDecoder ? binaryFileIcon : textFileIcon}
-              alt=''
-              className='right-circled'
-            />
-          </div>
-          <hr className='w-75 m-auto' />
           {textOrFile ? (
-            <textarea
-              cols='30'
-              rows='15'
-              name='ENC'
-              onChange={onChangeText}
-              value={!encoderOrDecoder ? DEC : ENC}></textarea>
+            <button
+              className='btn btn-outline-info m-2 middle-btn'
+              onClick={seeHuffmanTree}>
+              See Huffman Tree
+            </button>
           ) : (
-            <div className='w-100 d-flex flex-column align-items-center justify-content-center'>
-              <img src={binaryFileIcon} className='m-auto p-2 w-75' alt='' />
-
-              <div className='form-group p-1'>
-                <button type='submit' className='btn btn-primary m-auto'>
-                  Download Output File
-                </button>
-              </div>
-            </div>
+            ""
+          )}
+          {textOrFile ? (
+            <button
+              className='btn btn-outline-danger m-2 middle-btn'
+              onClick={clearBoth}>
+              Clear both
+            </button>
+          ) : (
+            ""
           )}
         </div>
+        <div className='col-md-5'>
+          <div className='card border-round'>
+            <div className='d-flex flex-row align-items-center'>
+              <div className='d-flex flex-column p-2'>
+                <h5 className='text-bold'>Output Text</h5>
+                <small>
+                  {!encoderOrDecoder
+                    ? "(Simple Plain English Text)"
+                    : "(Huffman Endoded Binary Text)"}
+                </small>
+              </div>
+              <img
+                src={encoderOrDecoder ? binaryFileIcon : textFileIcon}
+                alt=''
+                className='right-circled'
+              />
+            </div>
+            <hr className='w-75 m-auto' />
+            {textOrFile ? (
+              <textarea
+                cols='30'
+                rows='15'
+                name='ENC'
+                onChange={onChangeText}
+                disabled
+                value={!encoderOrDecoder ? DEC : ENC}></textarea>
+            ) : (
+              <div className='w-100 d-flex flex-column align-items-center justify-content-center'>
+                <img src={binaryFileIcon} className='m-auto p-2 w-75' alt='' />
+
+                <div className='form-group p-1'>
+                  <button type='submit' className='btn btn-primary m-auto'>
+                    Download Output File
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+
+        <LoadingModal isOpen={false} />
       </div>
-      <LoadingModal />
-    </div>
+      {showtree ? (
+        <div className='row'>
+          <div className='col-5'>
+            <h6>Huffman Tree Here</h6>
+            <br />
+            <pre>{JSON.stringify(tree, null, 2)}</pre>
+          </div>
+          <div className='col-7'>
+            <span className='badge badge-warning p-2'>Input File Size</span>
+            <span className='badge badge-warning p-2'>Output File Size</span>
+            <span className='badge badge-warning p-3'>Compression Ratio</span>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
 
--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -11,7 +11,7 @@ import {
 const LoadingModal = (props) => {
   const { buttonLabel, className } = props;
 
-  const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState(props.isOpen);
 
   const toggle = () => setModal(!modal);
 
@@ -25,7 +25,35 @@ const LoadingModal = (props) => {
         toggle={toggle}
         className='d-flex justify-content-center align-items-center'
         style={{ height: "80vh" }}>
-        <Spinner style={{ width: "5rem", height: "5rem", margin: "auto" }} />
+        <div className='row' style={{ padding: "10px" }}>
+          <div className='col-5'>
+            <h6>Huffman Tree Here</h6>
+            <pre>{JSON.stringify({}, null, 2)}</pre>
+          </div>
+          <div className='col-7'>
+            <span
+              className='badge badge-warning m-1 p-2'
+              style={{ minWidth: "140px" }}>
+              Input File Size
+            </span>{" "}
+            :<span className='badge badge-primary m-1 p-2'>Primary</span>
+            <br />
+            <span
+              className='badge badge-warning m-1 p-2'
+              style={{ minWidth: "140px" }}>
+              Output File Size
+            </span>{" "}
+            :<span className='badge badge-primary m-1 p-2'>Primary</span>
+            <br />
+            <span
+              className='badge badge-warning m-1 p-2'
+              style={{ minWidth: "140px" }}>
+              Compression Ratio
+            </span>
+            :<span className='badge badge-primary m-1 p-2'>Primary</span>
+            <br />
+          </div>
+        </div>
       </Modal>
     </div>
   );
