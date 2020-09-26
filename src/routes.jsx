37390cfa3e8b9cde98b1bab969f1369a1d648aef
Mayank-MP05 <mayank5pande@gmail.com>
2020-09-26 13:43:50 +0530

    Code cleaning at Router

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Home from "./components/home";
 import About from "./components/about";
 import Tabbed from "./components/tabbed";
 
