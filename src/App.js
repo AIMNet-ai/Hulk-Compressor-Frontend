983fb334b7f24fb11c3d520b010652eb7eb664f9
Mayank-MP05 <mayank5pande@gmail.com>
2020-09-26 12:14:05 +0530

    Code cleaning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Routes from "./routes";
@@ -15,19 +15,3 @@ function App() {
 }
 
 export default App;
-/*
-
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setcount(count + 1);
-          }}>
-          Add One
-        </button>
-      </header>
-    </div>
-
-*/
