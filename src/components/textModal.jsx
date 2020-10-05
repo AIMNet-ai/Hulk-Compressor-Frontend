8b31c045634c55d4847f37208cc4def7b09c043e
Mayank-MP05 <mayank5pande@gmail.com>
2020-10-05 20:10:16 +0530

    Hulk Loader in Text

--- src/components/textModal.jsx
+++ src/components/textModal.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Modal } from "reactstrap";
+import hulkLoading from "./../assets/hulk-loading.gif";
 
 const TextModal = (props) => {
   const { data, setmodalshow } = props;
+  const loadingTime = Math.random() * 3000;
+  const [loading, setloading] = useState(true);
   const [ipOpen, setipOpen] = useState(props.isOpen);
   const toggle = () => {
     setipOpen(!ipOpen);
     setmodalshow(!ipOpen);
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setloading(false);
+    }, loadingTime);
+  }, []);
 
   return (
     <div>
@@ -16,45 +25,51 @@ const TextModal = (props) => {
         toggle={toggle}
         className='d-flex justify-content-center align-items-center'
         style={{ height: "80vh" }}>
-        <Button color='danger' onClick={toggle}>
-          Close
-        </Button>
-        <div
-          className='row justify-content-center align-items-center'
-          style={{ padding: "10px" }}>
-          <div className='col-4'>
-            <h6>Huffman Tree Here</h6>
-            <pre>{JSON.stringify(data.tree, null, 2)}</pre>
-          </div>
-          <div className='col-8 justify-content-center align-items-center'>
-            <span
-              className='badge badge-warning m-1 p-2'
-              style={{ minWidth: "140px" }}>
-              Input File Size
-            </span>{" "}
-            :
-            <span className='badge badge-primary m-1 p-2'>{`${data.inputSize} bytes`}</span>
-            <br />
-            <span
-              className='badge badge-warning m-1 p-2'
-              style={{ minWidth: "140px" }}>
-              Output File Size
-            </span>{" "}
-            :
-            <span className='badge badge-primary m-1 p-2'>{`${data.outputSize} bytes`}</span>
-            <br />
-            <span
-              className='badge badge-warning m-1 p-2'
-              style={{ minWidth: "140px" }}>
-              Compression Ratio
-            </span>
-            :
-            <span className='badge badge-primary m-1 p-2'>
-              {data.compressionRatio}
-            </span>
-            <br />
-          </div>
-        </div>
+        {loading ? (
+          <img src={hulkLoading} className='w-100' />
+        ) : (
+          <>
+            <Button color='danger' onClick={toggle}>
+              Close
+            </Button>
+            <div
+              className='row justify-content-center align-items-center'
+              style={{ padding: "10px" }}>
+              <div className='col-4'>
+                <h6>Huffman Tree Here</h6>
+                <pre>{JSON.stringify(data.tree, null, 2)}</pre>
+              </div>
+              <div className='col-8 justify-content-center align-items-center'>
+                <span
+                  className='badge badge-warning m-1 p-2'
+                  style={{ minWidth: "140px" }}>
+                  Input File Size
+                </span>{" "}
+                :
+                <span className='badge badge-primary m-1 p-2'>{`${data.inputSize} bytes`}</span>
+                <br />
+                <span
+                  className='badge badge-warning m-1 p-2'
+                  style={{ minWidth: "140px" }}>
+                  Output File Size
+                </span>{" "}
+                :
+                <span className='badge badge-primary m-1 p-2'>{`${data.outputSize} bytes`}</span>
+                <br />
+                <span
+                  className='badge badge-warning m-1 p-2'
+                  style={{ minWidth: "140px" }}>
+                  Compression Ratio
+                </span>
+                :
+                <span className='badge badge-primary m-1 p-2'>
+                  {data.compressionRatio}
+                </span>
+                <br />
+              </div>
+            </div>
+          </>
+        )}
       </Modal>
     </div>
   );
