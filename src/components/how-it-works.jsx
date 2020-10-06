6ad8aded5bc3cbee41f65c16368b79e717e6526a
Mayank-MP05 <mayank5pande@gmail.com>
2020-10-06 10:20:24 +0530

    How it works route added

---
+++ src/components/how-it-works.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HowItWorks() {
+  return (
+    <div className='container'>
+      <h2>How it works</h2>
+    </div>
+  );
+}
+
+export default HowItWorks;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
+
 import About from "./components/about";
+import HowItWorks from "./components/how-it-works";
 import Tabbed from "./components/tabbed";
 
 function Routes() {
@@ -8,6 +10,7 @@ function Routes() {
     <Switch>
       <Route exact path='/home' component={Tabbed} />
       <Route exact path='/about' component={About} />
+      <Route exact path='/how-it-works' component={HowItWorks} />
       <Redirect from='/' to='/home' />
     </Switch>
   );
