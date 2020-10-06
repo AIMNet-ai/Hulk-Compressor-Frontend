942d3b4447d93d4cc7e0f5121211451205756a34
Mayank-MP05 <mayank5pande@gmail.com>
2020-10-06 10:30:05 +0530

    How it works Iframe working properly

--- src/components/how-it-works.jsx
+++ src/components/how-it-works.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 function HowItWorks() {
   return (
-    <div className='container'>
-      <h2>How it works</h2>
+    <div className='m-2'>
+      <h2>Draw a Tree using Huffman Coding</h2>
+      <hr className='m-1' />
+      <iframe
+        src='https://people.ok.ubc.ca/ylucet/DS/Huffman.html'
+        frameBorder='0'
+        title='Huffman Frame'
+        width={window.innerWidth * 0.9}
+        height={window.innerWidth * 0.7}></iframe>
     </div>
   );
 }
