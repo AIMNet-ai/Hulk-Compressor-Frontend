ded92214c697345595d02291d017d0bde60d769d
Mayank-MP05 <mayank5pande@gmail.com>
2020-10-06 10:37:15 +0530

    About us Roll No. added

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -15,8 +15,8 @@ function About() {
         border: "1px solid black",
         boxShadow: "5px 5px 5px black",
       }}>
-      <div className='row' style={{ padding: "45px" }}>
-        <div className='col-7' style={{ padding: "35px 10px" }}>
+      <div className='row' style={{ padding: "25px" }}>
+        <div className='col-7' style={{ padding: "25px 10px" }}>
           <h4>
             <span className='p-1 badge badge-primary'>Project Title</span>Image
             and Text compression and encryption using hybrid approach
@@ -29,7 +29,30 @@ function About() {
             data. This hybrid approach is combination of Dynamic Bit reduction
             method and Huffman coding.
           </p>
-          <Link className='btn btn-warning' style={{ borderRadius: "50vh" }}>
+
+          <ul className='list-group'>
+            <li className='list-group-item d-flex flex-row'>
+              Jayesh Shelar
+              <span className='badge badge-primary p-1 ml-auto'>
+                TECOB254
+              </span>{" "}
+            </li>
+            <li className='list-group-item d-flex flex-row'>
+              Dhanashree Bhandari{" "}
+              <span className='badge badge-primary p-1 ml-auto'>TECOB203</span>
+            </li>
+            <li className='list-group-item d-flex flex-row'>
+              Mayank Pachpande{" "}
+              <span className='badge badge-primary p-1 ml-auto'> TECOB230</span>
+            </li>
+            <li className='list-group-item d-flex flex-row'>
+              Dipak Ghatole{" "}
+              <span className='badge badge-primary p-1 ml-auto'>TECOB206</span>
+            </li>
+          </ul>
+          <Link
+            className='btn btn-warning m-2'
+            style={{ borderRadius: "50vh" }}>
             How it Works...
           </Link>
         </div>
