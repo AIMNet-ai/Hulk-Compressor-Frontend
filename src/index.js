347bed8d4479012f0c377da020c172b79fb45ca3
Mayank-MP05 <mayank5pande@gmail.com>
2021-05-07 12:05:47 +0530

    Cached the deprecated pkg of fortawesome

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./assets/fortawesome.css";
+import "./assets/fa-cached/css/all.min.css";
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./comman.css";
